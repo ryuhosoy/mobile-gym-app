dba83e3ff9efbd049dd4aacbf9a5d792b92dfaa7
ryuhosoy <longpingxigu@gmail.com>
2025-03-07 21:47:20 +0900

    ユーザーセレクト画面を追加、画面戻れるように修正

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-import { useColorScheme } from 'react-native';
-import { LocationProvider } from './contexts/LocationContext';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import "react-native-reanimated";
+import { useColorScheme } from "react-native";
+import { LocationProvider } from "./contexts/LocationContext";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -14,7 +18,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -29,18 +33,11 @@ export default function RootLayout() {
 
   return (
     <LocationProvider>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
-          <Stack.Screen
-            name="index"
-            options={{
-              title: '',
-              headerStyle: {
-                backgroundColor: '#6B4DE6',
-              },
-              headerTintColor: '#fff',
-            }}
-          />
+          <Stack.Screen name="index" options={{ title: "セレクト" }} />
+          <Stack.Screen name="user/gymSearch" options={{ title: "ジム検索" }} />
+          <Stack.Screen name="gym/[id]" options={{ title: "ジム詳細" }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
