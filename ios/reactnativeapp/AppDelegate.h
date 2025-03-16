b3084289591db7147a48f34b81fdf5052efc6999
ryuhosoy <longpingxigu@gmail.com>
2025-03-16 23:13:01 +0900

    ios build

---
+++ ios/reactnativeapp/AppDelegate.h
@@ -0,0 +1,8 @@
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+#import <Expo/Expo.h>
+
+/Users/ryuhosoy/Desktop/develop-apps/gym-app/ios/reactnativeapp/AppDelegate.h
+@interface AppDelegate : EXAppDelegateWrapper
+
+@end
